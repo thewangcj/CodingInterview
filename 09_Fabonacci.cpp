2984e00d8d7ac439a9a751afcd7b3a74da323cb7
thewangcj <thewangcj@gmail.com>
2018-01-17 17:01:42 +0800

    solve problem 8、9、10

---
+++ 09_Fabonacci.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+long long Fabonacci(unsigned const int n) {
+  int result[2] = {0, 1};
+  if(n < 2) {
+    return result[n];
+  }
+
+  long long fib_one = 0;
+  long long fib_two = 1;
+  long long fib_n = 0;
+  for(unsigned int i = 2; i <= n; i++) {
+    fib_n = fib_one + fib_two;
+    fib_one = fib_two;
+    fib_two = fib_n;
+  }
+  return fib_n;
+}
+
+int main(int argc, char const *argv[]) {
+  cout<< Fabonacci(2) << endl;;
+  cout<< Fabonacci(48) << endl;
+  cout<< Fabonacci(49) << endl;
+  cout<< Fabonacci(50) << endl;
+
+  return 0;
+}
---
+++ 10_NumOfOne.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int NumOfOneSolutionOne(int n) {
+  int num = 0;
+  unsigned int flag = 1;
+  while ( flag ) {
+    if(n & flag) {
+      num++;
+    }
+    flag = flag << 1;
+  }
+  return num;
+}
+
+int NumOfOneSolutionTwo(int n) {
+  int num = 0;
+  while ( n ) {
+    n = (n - 1) & n;
+    num++;
+  }
+  return num;
+}
+
+void Test(int n, int expect) {
+  if(NumOfOneSolutionOne(n) == expect && NumOfOneSolutionTwo(n) == expect) {
+    cout<< "test pass" << endl;
+    return;
+  } else {
+    cout<< "solution_one:" << NumOfOneSolutionOne(n)
+        << " solution_two:" << NumOfOneSolutionTwo(n)
+        << " expect:" << expect <<" test fail" << endl;
+    return;
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  // 输入0，期待的输出是0
+  Test(0, 0);
+
+  // 输入1，期待的输出是1
+  Test(1, 1);
+
+  // 输入10，期待的输出是2
+  Test(10, 2);
+
+  // 输入0x7FFFFFFF，期待的输出是31
+  Test(0x7FFFFFFF, 31);
+
+  // 输入0xFFFFFFFF（负数），期待的输出是32
+  Test(0xFFFFFFFF, 32);
+
+  // 输入0x80000000（负数），期待的输出是1
+  Test(0x80000000, 1);
+  return 0;
+}
