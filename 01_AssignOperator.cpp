7390870127c7605667823fefd2bede25eee902d4
thewangcj <thewangcj@gmail.com>
2020-08-21 20:52:59 +0800

    面试题3

--- 01_AssignOperator.cpp
+++ 01_AssignOperator.cpp
@@ -7,61 +7,130 @@ using namespace std;
 class CMyString
 {
 public:
-  CMyString(const char* pDate = NULL);
-  CMyString(const CMyString& str);
-  CMyString& operator=(const CMyString& str);
+  CMyString(const char *pDate = NULL);
+  CMyString(const CMyString &str);
+  CMyString &operator=(const CMyString &str);
   ~CMyString(void);
-  void print() const;
+  void Print() const;
+
 private:
-  char * m_pDate;
+  char *m_pData;
 };
 
-inline CMyString::CMyString(const char* pDate)
+inline CMyString::CMyString(const char *pDate)
 {
-  if(pDate) {
-    m_pDate = new char[strlen(pDate) + 1];
-    strcpy(m_pDate,pDate);
+  if (pDate)
+  {
+    m_pData = new char[strlen(pDate) + 1];
+    strcpy(m_pData, pDate);
   }
-  else {
-    m_pDate = new char[1];
-    *m_pDate = '\0';
+  else
+  {
+    m_pData = new char[1];
+    *m_pData = '\0';
   }
 }
 
-inline CMyString::CMyString(const CMyString& str)
+inline CMyString::CMyString(const CMyString &str)
 {
-  m_pDate = new char[strlen(str.m_pDate) + 1];
-  strcpy(m_pDate,str.m_pDate);
+  m_pData = new char[strlen(str.m_pData) + 1];
+  strcpy(m_pData, str.m_pData);
 }
 
-CMyString& CMyString::operator = (const CMyString& str)
+CMyString &CMyString::operator=(const CMyString &str)
 {
-  if(this == &str){
+  if (this == &str)
+  {
     return *this;
   }
-  else{
-    delete[] m_pDate;
-    m_pDate = new char[strlen(str.m_pDate) + 1];
-    strcpy(m_pDate,str.m_pDate);
+  else
+  {
+    // delete[] m_pData;
+    // m_pData = new char[strlen(str.m_pData) + 1];
+    // strcpy(m_pData, str.m_pData);
+    // return *this;
+    CMyString strTemp(str);
+    char* pTemp = strTemp.m_pData;
+    strTemp.m_pData = m_pData;
+    m_pData = pTemp;
     return *this;
   }
 }
 
 CMyString::~CMyString()
 {
-  delete[] m_pDate;
+  delete[] m_pData;
 }
 
-void CMyString::print() const
+// ====================测试代码====================
+void CMyString::Print() const
 {
-  cout<<m_pDate<<endl;
+  printf("%s", m_pData);
 }
 
-int main()
+void Test1()
 {
-  CMyString a("hello");
-  CMyString b = a;
-  a.print();
-  b.print();
+  printf("Test1 begins:\n");
+
+  const char* text = "Hello world";
+
+  CMyString str1(text);
+  CMyString str2;
+  str2 = str1;
+
+  printf("The expected result is: %s.\n", text);
+
+  printf("The actual result is: ");
+  str2.Print();
+  printf(".\n");
+}
+
+// 赋值给自己
+void Test2()
+{
+  printf("Test2 begins:\n");
+
+  const char* text = "Hello world";
+
+  CMyString str1(text);
+  str1 = str1;
+
+  printf("The expected result is: %s.\n", text);
+
+  printf("The actual result is: ");
+  str1.Print();
+  printf(".\n");
+}
+
+// 连续赋值
+void Test3()
+{
+  printf("Test3 begins:\n");
+
+  const char* text = "Hello world";
+
+  CMyString str1(text);
+  CMyString str2, str3;
+  str3 = str2 = str1;
+
+  printf("The expected result is: %s.\n", text);
+
+  printf("The actual result is: ");
+  str2.Print();
+  printf(".\n");
+
+  printf("The expected result is: %s.\n", text);
+
+  printf("The actual result is: ");
+  str3.Print();
+  printf(".\n");
+}
+
+int main(int argc, char* argv[])
+{
+  Test1();
+  Test2();
+  Test3();
+
   return 0;
 }
--- 02_Sinlgleton.cpp
+++ 02_Sinlgleton.cpp
@@ -7,12 +7,19 @@ using namespace std;
 class Singleton1
 {
 public:
-  static Singleton1& getInstance(){
+  static Singleton1& getInstance()
+  {
     return instance;
   }
+
 private:
-  Singleton1(){};
-  Singleton1(const Singleton1&);
+  Singleton1(){
+    cout << "Singleton1 构造" << endl;
+  };
+  ~Singleton1(){
+    cout << "Singleton1 析构" << endl;
+  }
+  Singleton1(const Singleton1 &);
   static Singleton1 instance;
 };
 Singleton1 Singleton1::instance;
@@ -21,20 +28,41 @@ class Singleton2
 {
 public:
   //只有当需要这个单例对象的时候才创建
-  static Singleton2& getInstance(){
+  static Singleton2 &getInstance()
+  {
     static Singleton2 instance;
     return instance;
   }
+
 private:
-  Singleton2(){};
-  Singleton2(const Singleton2&);
+  Singleton2(){
+    cout << "Singleton2 构造" << endl;
+  };
+  ~Singleton2(){
+    cout << "Singleton2 析构" << endl;
+  }
+  Singleton2(const Singleton2 &);
 };
 
+void test1()
+{
+  cout << "单例模式 singleton1 访问前" << endl;
+  Singleton1 *singleton1 = &Singleton1::getInstance();
+  Singleton1 *singleton2 = &Singleton1::getInstance();
+  cout << (singleton1 == singleton2) << '\n';
+}
+
+void test2()
+{
+  cout << "单例模式 singleton2 访问前" << endl;
+  Singleton2 *singleton1 = &Singleton2::getInstance();
+  Singleton2 *singleton2 = &Singleton2::getInstance();
+  cout << (singleton1 == singleton2) << '\n';
+}
 
 int main()
 {
-  Singleton2* singleton1 = &Singleton2::getInstance();
-  Singleton2* singleton2 = &Singleton2::getInstance();
-  cout << (singleton1==singleton2) << '\n';
+  test1();
+  test2();
   return 0;
 }
